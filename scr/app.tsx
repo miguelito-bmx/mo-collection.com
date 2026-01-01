4e13db0e140e00c7b5c81cff77199605ab1cd04e
bruce miguel <brucemiguelx@gmail.com>
2026-01-01 12:16:40 +0300

    Update app.tsx

--- scr/app.tsx
+++ scr/app.tsx
@@ -1,1 +1,44 @@
 
+import { useState } from 'react';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Products from './components/Products';
+import Chamas from './components/Chamas';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import WhatsAppButton from './components/WhatsAppButton';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home onNavigate={setCurrentPage} />;
+      case 'products':
+        return <Products onNavigate={setCurrentPage} />;
+      case 'chamas':
+        return <Chamas />;
+      case 'about':
+        return <About />;
+      case 'contact':
+        return <Contact />;
+      default:
+        return <Home onNavigate={setCurrentPage} />;
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col bg-white">
+      <Navigation currentPage={currentPage} onNavigate={setCurrentPage} />
+      <main className="flex-grow">
+        {renderPage()}
+      </main>
+      <Footer />
+      <WhatsAppButton />
+    </div>
+  );
+}
+
+export default App;
