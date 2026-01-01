951ca7e443c54782b8026063fac072a08e18f550
bruce miguel <brucemiguelx@gmail.com>
2026-01-01 09:53:52 +0300

    Update main.tsx

--- scr/components/main.tsx
+++ scr/components/main.tsx
@@ -1,3 +1,10 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
